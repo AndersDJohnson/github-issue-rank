008d8c2cb63391b2be5b2cbfd0b0bee96058b326
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-09-28 22:26:21 -0500

    Add tests for unicode thumbs up.

--- test/index.js
+++ test/index.js
@@ -8,7 +8,8 @@ assert(ghir.hasVote('I agree, +1!'));
 assert(! ghir.hasVote('I agree, 2+1!'));
 assert(! ghir.hasVote('I agree!'));
 
-// assert(ghir.hasVote(' \uD83D\uDC4Db '));
-// assert(ghir.hasVote('a\uD83D\uDC4Db'));
-// assert(ghir.hasVote('a👍'));
-// assert(ghir.hasVote('👍b'));
+assert(ghir.hasVote(' \uD83D\uDC4D '));
+assert(ghir.hasVote(' \uD83D\uDC4D b '));
+assert(ghir.hasVote('a \uD83D\uDC4D b'));
+assert(ghir.hasVote('a 👍'));
+assert(ghir.hasVote('👍 b'));
