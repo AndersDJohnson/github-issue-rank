9ef7a4a1976f4fc58d3f351a88a0ce0d65dcb2ea
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-10-01 14:48:37 -0500

    Octokat cache handler from gh-board.

--- src/index.js
+++ src/index.js
@@ -240,30 +240,60 @@
 
           console.log('githubAccessToken', githubAccessToken);
 
-          var storage = window.localStorage;
+
+          /**
+           * https://github.com/philschatz/gh-board/blob/master/src/github-client.js#L7-L66
+           */
+          var cacheHandler = new (class CacheHandler {
+            constructor() {
+              // Pull data from `sessionStorage`
+              // this.storage = window.sessionStorage;
+              this.storage = window.localStorage;
+            }
+            get(method, path) {
+              var key = method + ' ' + path;
+              var ret = this.storage.getItem(key);
+              if (! ret) return null;
+              ret = JSON.parse(ret);
+              var {data, linkRelations} = ret;
+              _.each(linkRelations, (value, key) => {
+                if (value) {
+                  data[key] = value;
+                }
+              });
+              return ret;
+            }
+            add(method, path, eTag, data, status) {
+              var linkRelations = {};
+              // if data is an array, it contains additional link relations (to other pages)
+              if (_.isArray(data)) {
+                _.each(['next', 'previous', 'first', 'last'], (name) => {
+                  var key = name + '_page_url';
+                  if (data[key]) {
+                    linkRelations[key] = data[key];
+                  }
+                });
+              }
+
+              var key = method + ' ' + path;
+              var cached = {eTag, data, status, linkRelations};
+              cached = JSON.stringify(cached);
+
+              try {
+                this.storage.setItem(key, cached);
+              } catch (e) {
+                console.error(e);
+              }
+            }
+          });
+
 
           octo = new Octokat({
             // username: "USER_NAME",
             // password: "PASSWORD"
             //
             token: githubAccessToken,
-            cacheHandler: {
-              get: function (method, path) {
-                console.log('get', arguments);
-                var data = storage.getItem(method + ':' + path);
-                if (data == null) return null;
-                return JSON.parse(data);
-              },
-              add: function (method, path, eTag, data, status) {
-                console.log('add', arguments);
-                try {
-                  storage.setItem(method + ':' + path, JSON.stringify(data));
-                }
-                catch(e) {
-                  console.error('Error setting storage', e);
-                }
-              }
-            }
+            cacheHandler: cacheHandler
           });
 
           postAuth();
