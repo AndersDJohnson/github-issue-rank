a3d8e11f28bef303bc121f160569640e1ff94f6f
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-11-04 07:50:28 -0600

    Server cors.

--- server/index.js
+++ server/index.js
@@ -2,15 +2,24 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 
-app.use(cors({
+var corsed = cors({
   allowedHeaders: [
     'Link',
     'ETag',
     'If-Modified-Since',
+    'If-None-Match',
     'Access-Control-Allow-Headers',
     'Authorization'
+  ],
+  exposedHeaders: [
+    'Link',
+    'ETag'
   ]
-}));
+});
+
+app.options('*', corsed);
+
+app.use(corsed);
 
 app.use(express.static(__dirname + '/cache'));
 
